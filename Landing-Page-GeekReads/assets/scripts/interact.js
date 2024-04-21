4e91340ecae9173167a206b9f27889fdb0bf2972
rimbacakra12 <rimbacakra910@gmail.com>
2024-04-21 12:01:40 +0700

    Add files via upload

---
+++ Landing-Page-GeekReads/assets/scripts/interact.js
@@ -0,0 +1,34 @@
+function topScroll () {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dots");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
